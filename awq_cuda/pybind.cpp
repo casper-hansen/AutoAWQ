97af18e4ba3703ebacbf145abe4d82403ce612bc
Casper Hansen <casperbh.96@gmail.com>
2023-09-06 08:58:43 +0000

    Update rotary embedding kernel

--- awq_cuda/pybind.cpp
+++ awq_cuda/pybind.cpp
@@ -8,5 +8,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     m.def("layernorm_forward_cuda", &layernorm_forward_cuda, "FasterTransformer layernorm kernel");
     m.def("gemm_forward_cuda", &gemm_forward_cuda, "Quantized GEMM kernel.");
-    m.def("rotary_embedding_neox", &rotary_embedding_neox, "Apply GPT-NeoX style rotary embedding to query and key");
+    m.def("rotary_embedding", &rotary_embedding, "Apply rotary embedding to query and key");
 }
