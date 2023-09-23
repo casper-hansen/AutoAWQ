e418553bb726f3b5bb96c250ac63217e2c4d9025
Casper Hansen <casperbh.96@gmail.com>
2023-09-23 21:58:01 +0000

    Install cutlass script update

--- scripts/install_cutlass.sh
+++ scripts/install_cutlass.sh
@@ -3,5 +3,7 @@ export CUDACXX=/usr/local/cuda/bin/nvcc
 cd submodules/cutlass
 rm -rf build
 mkdir -p build && cd build
-cmake .. -DCUTLASS_NVCC_ARCHS="80;86;89;90" -DCUTLASS_ENABLE_TESTS=OFF -DCUTLASS_UNITY_BUILD_ENABLED=ON
+
+# NOTE: You can build for all Ampere+Hopper with 80;86;89;90
+cmake .. -DCUTLASS_NVCC_ARCHS="80" -DCUTLASS_ENABLE_TESTS=OFF -DCUTLASS_UNITY_BUILD_ENABLED=ON
 make -j $(nproc)
