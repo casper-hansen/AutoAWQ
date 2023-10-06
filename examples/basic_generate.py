204a3a12373c23fa652a7c2762ff65173d025b37
Casper Hansen <casperbh.96@gmail.com>
2023-10-06 13:59:48 +0000

    Update Mistral example

--- examples/basic_generate.py
+++ examples/basic_generate.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, TextStreamer
 quant_path = "TheBloke/Mistral-7B-OpenOrca-AWQ"
 
 # Load model
-model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=False, safetensors=True)
+model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=True, safetensors=True)
 tokenizer = AutoTokenizer.from_pretrained(quant_path, trust_remote_code=True)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
@@ -16,8 +16,12 @@ You are MistralOrca, a large language model trained by Alignment Lab AI. Write o
 {prompt}<|im_end|>
 <|im_start|>assistant"""
 
+prompt = "You're standing on the surface of the Earth. "\
+        "You walk one mile south, one mile west and one mile north. "\
+        "You end up exactly where you started. Where are you?"
+
 tokens = tokenizer(
-    prompt_template.format(prompt="Why is ice cream so good, yes so good?"), 
+    prompt_template.format(prompt=prompt), 
     return_tensors='pt'
 ).input_ids.cuda()
 
