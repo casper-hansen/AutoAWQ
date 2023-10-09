9104e9be46a8cb528aab9684d27ebb36ff1a73fc
Casper Hansen <casperbh.96@gmail.com>
2023-10-09 16:02:06 +0000

    Refactor module

--- awq/modules/linear.py
+++ awq/modules/linear.py
@@ -210,70 +210,49 @@ class WQLinear_GEMV(nn.Module):
             self.in_features, self.out_features, self.bias is not None, self.w_bit, self.group_size
         )
 
-class WQLinear_W8A8(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, act_quant='per_token', quantize_output=False):
+class WQLinear_FakeW8A8(nn.Module):
+    def __init__(self, in_features, out_features, act_quant='per_token', quantize_output=False, w_bit=8):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.w_bit = w_bit
 
         self.register_buffer('weight', torch.randn(
             self.out_features, self.in_features, dtype=torch.float16, requires_grad=False)
         )
-        if bias:
-            self.register_buffer('bias', torch.zeros(
-                (1, self.out_features), dtype=torch.float16, requires_grad=False)
-            )
-        else:
-            self.register_buffer('bias', None)
 
         if act_quant == 'per_token':
-            self.act_quant_name = 'per_token'
-            self.act_quant = partial(quantize_activation_per_token_absmax, n_bits=8)
-
+            self.quant_function = partial(quantize_activation_per_token_absmax, n_bits=self.w_bit)
         elif act_quant == 'per_tensor':
-            self.act_quant_name = 'per_tensor'
-            self.act_quant = partial(quantize_activation_per_tensor_absmax, n_bits=8)
+            self.quant_function = partial(quantize_activation_per_tensor_absmax, n_bits=self.w_bit)
 
         if quantize_output:
-            self.output_quant_name = self.act_quant_name
-            self.output_quant = self.act_quant
+            self.quant_output_function = self.quant_function
         else:
-            self.output_quant_name = 'None'
-            self.output_quant = lambda x: x
+            self.quant_output_function = lambda x: x
 
     def to(self, *args, **kwargs):
-        super(WQLinear_W8A8, self).to(*args, **kwargs)
+        super(WQLinear_FakeW8A8, self).to(*args, **kwargs)
         self.weight = self.weight.to(*args, **kwargs)
-        if self.bias is not None:
-            self.bias = self.bias.to(*args, **kwargs)
         return self
-
-    @torch.no_grad()
-    def forward(self, x):
-        q_x = self.act_quant(x)
-        y = torch.functional.F.linear(q_x, self.weight, self.bias)
-        q_y = self.output_quant(y)
-        return q_y
-
+    
     @staticmethod
-    def from_float(module, weight_quant='per_channel', act_quant='per_token', quantize_output=False):
-        assert isinstance(module, torch.nn.Linear)
-        new_module = WQLinear_W8A8(
-            module.in_features, module.out_features, module.bias is not None, 
-            act_quant=act_quant, quantize_output=quantize_output
+    def from_linear(module: nn.Linear, weight_quant='per_channel', act_quant='per_token', quantize_output=False, w_bit=8):
+        new_module = WQLinear_FakeW8A8(
+            module.in_features, module.out_features, act_quant=act_quant,
+            quantize_output=quantize_output, w_bit=w_bit
         )
         
         if weight_quant == 'per_channel':
             new_module.weight = quantize_weight_per_channel_absmax(module.weight, n_bits=8)
         elif weight_quant == 'per_tensor':
             new_module.weight = quantize_weight_per_tensor_absmax(module.weight, n_bits=8)
-
-        new_module.weight_quant_name = weight_quant
-
-        if module.bias is not None:
-            new_module.bias = module.bias
         
         return new_module
 
-    def __repr__(self):
-        return f'W8A8Linear({self.in_features}, {self.out_features}, bias={self.bias is not None}, weight_quant={self.weight_quant_name}, act_quant={self.act_quant_name}, output_quant={self.output_quant_name})'
+    @torch.no_grad()
+    def forward(self, x):
+        x_quantized = self.quant_function(x)
+        output = torch.functional.F.linear(x_quantized, self.weight)
+        output_quantized = self.quant_output_function(output)
+        return output_quantized
--- awq/quantize/quantizer.py
+++ awq/quantize/quantizer.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from awq.utils.utils import clear_memory
 from awq.utils.calib_data import get_calib_dataset
 from awq.quantize.scale import apply_scale, apply_clip
-from awq.modules.linear import WQLinear_GEMM, WQLinear_GEMV, WQLinear_W8A8
+from awq.modules.linear import WQLinear_GEMM, WQLinear_GEMV, WQLinear_FakeW8A8
 from awq.quantize.scale import allowed_norms, scale_ln_fcs, scale_fc_fcs, extract_scales
 from awq.utils.module import append_str_prefix, get_op_name, get_named_linears, set_op_by_name
 
@@ -383,8 +383,9 @@ class SmoothQuantizer:
                 linear_layer = linear_layer.cuda()
 
                 quantize_output = name in self.awq_model.quantize_output
-                q_linear = WQLinear_W8A8.from_float(
-                    linear_layer, self.weight_quant, self.act_quant, quantize_output=quantize_output
+                q_linear = WQLinear_FakeW8A8.from_linear(
+                    linear_layer, self.weight_quant, self.act_quant, 
+                    quantize_output=quantize_output, w_bit=self.w_bit
                 )
 
                 linear_layer.cpu()
