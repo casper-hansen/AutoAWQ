c8502f7aaf124f304617007bf397a3de47032153
Casper <casperbh.96@gmail.com>
2023-11-02 20:45:02 +0100

    Upgrade to CUDA 12, torch 2.1.0

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ from torch.utils.cpp_extension import BuildExtension, CUDA_HOME, CUDAExtension
 
 os.environ["CC"] = "g++"
 os.environ["CXX"] = "g++"
+AUTOAWQ_VERSION = "0.1.6"
+PYPI_BUILD = os.getenv("PYPI_BUILD", "0") == "1"
 
-try:
-    CUDA_VERSION = "".join(os.environ.get("CUDA_VERSION", torch.version.cuda).split("."))
-except Exception as ex:
-    raise RuntimeError("Your system must have an Nvidia GPU for installing AutoAWQ")
+if not PYPI_BUILD:
+    try:
+        CUDA_VERSION = "".join(os.environ.get("CUDA_VERSION", torch.version.cuda).split("."))[:3]
+        AUTOAWQ_VERSION += f"cu+{CUDA_VERSION}"
+    except Exception as ex:
+        raise RuntimeError("Your system must have an Nvidia GPU for installing AutoAWQ")
 
 common_setup_kwargs = {
-    "version": f"0.1.6+cu{CUDA_VERSION}",
+    "version": AUTOAWQ_VERSION,
     "name": "autoawq",
     "author": "Casper Hansen",
     "license": "MIT",
@@ -39,7 +43,7 @@ common_setup_kwargs = {
 }
 
 requirements = [
-    "torch>=2.0.0",
+    "torch>=2.1.0",
     "transformers>=4.34.0",
     "tokenizers>=0.12.1",
     "accelerate",
