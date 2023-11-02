0c8d3abd8d8fe594186dfe38245764273a82632c
Casper <casperbh.96@gmail.com>
2023-11-02 20:34:17 +0100

    Improve download script

--- scripts/download_wheels.sh
+++ scripts/download_wheels.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Set the GitHub release URL
-RELEASE_URL="https://api.github.com/repos/casper-hansen/AutoAWQ/releases/tags/v0.0.1"
+# Set variables
+AWQ_VERSION="0.1.6"
+CUDA_VERSION="cu1180"
+RELEASE_URL="https://api.github.com/repos/casper-hansen/AutoAWQ/releases/tags/v${AWQ_VERSION}"
 
 # Create a directory to download the wheels
 mkdir -p dist
@@ -10,12 +12,21 @@ cd dist
 # Download all the wheel files from the GitHub release
 curl -s $RELEASE_URL | \
     jq -r ".assets[].browser_download_url" | \
-    grep .whl | \
+    grep '\.whl' | \
+    grep "$CUDA_VERSION" | \
     xargs -n 1 wget
 
 # Rename the wheels from 'linux_x86_64' to 'manylinux_x86_64'
-for file in *linux_x86_64.whl; do
-    mv "$file" "$(echo $file | sed 's/linux_x86_64/manylinux2014_x86_64/')"
+# Remove CUDA version from filename
+for file in *"$CUDA_VERSION"*.whl; do
+    # First, rename the platform from 'linux_x86_64' to 'manylinux2014_x86_64'
+    intermediate_name=$(echo "$file" | sed 's/linux_x86_64/manylinux2014_x86_64/')
+    
+    # Then, remove the CUDA version from the filename
+    newname=$(echo "$intermediate_name" | sed "s/+${CUDA_VERSION}//")
+    
+    mv -v "$file" "$newname"
 done
+
 
 cd ..
