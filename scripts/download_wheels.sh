9f2c383f589936b0732c9dff77ac9d92b96d25cb
Casper <casperbh.96@gmail.com>
2023-11-05 15:17:08 +0100

    Exclude download of CUDA wheels (#159)

--- scripts/download_wheels.sh
+++ scripts/download_wheels.sh
@@ -9,10 +9,12 @@ mkdir -p dist
 cd dist
 
 # Download all the wheel files from the GitHub release
+# excluding ones with '+cu' (%2B is + but encoded)
 curl -s $RELEASE_URL | \
     jq -r ".assets[].browser_download_url" | \
     grep '\.whl' | \
-    xargs -n 1 wget
+    grep -v '%2Bcu' | \
+    xargs -n 1 -P 4 wget
 
 # Rename the wheels from 'linux_x86_64' to 'manylinux_x86_64'
 for file in *linux_x86_64.whl; do
