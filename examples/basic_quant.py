1c61aa38b6d7cdcea5986790f40b9a70e016f67e
Casper Hansen <casperbh.96@gmail.com>
2023-12-11 16:17:34 +0000

    Update example for Mixtral

--- examples/basic_quant.py
+++ examples/basic_quant.py
@@ -1,13 +1,14 @@
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 
-model_path = 'mixtral-moe'
-quant_path = 'mixtral-moe-awq'
+model_path = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
+quant_path = 'mixtral-instruct-awq'
 quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
 
 # Load model
 # NOTE: pass safetensors=True to load safetensors
-model = AutoAWQForCausalLM.from_pretrained(model_path, trust_remote_code=True, **{"low_cpu_mem_usage": True})
+model = AutoAWQForCausalLM.from_pretrained(model_path, safetensors=True,
+                                            trust_remote_code=True, **{"low_cpu_mem_usage": True})
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 # Quantize
