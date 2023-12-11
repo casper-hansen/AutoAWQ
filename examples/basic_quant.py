7249119f687ee5af7bda6230f91a427d5465dbba
Casper Hansen <casperbh.96@gmail.com>
2023-12-11 18:14:44 +0000

    Mixtral quant works

--- examples/basic_quant.py
+++ examples/basic_quant.py
@@ -3,16 +3,19 @@ from transformers import AutoTokenizer
 
 model_path = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
 quant_path = 'mixtral-instruct-awq'
-quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+modules_to_not_convert = ["gate"]
+quant_config = {
+    "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM",
+    "modules_to_not_convert": modules_to_not_convert
+}
 
 # Load model
 # NOTE: pass safetensors=True to load safetensors
-model = AutoAWQForCausalLM.from_pretrained(model_path, safetensors=True,
-                                            trust_remote_code=True, **{"low_cpu_mem_usage": True})
+model = AutoAWQForCausalLM.from_pretrained(model_path, safetensors=True, **{"low_cpu_mem_usage": True})
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 # Quantize
-model.quantize(tokenizer, quant_config=quant_config)
+model.quantize(tokenizer, quant_config=quant_config, modules_to_not_convert=modules_to_not_convert)
 
 # Save quantized model
 model.save_quantized(quant_path)
