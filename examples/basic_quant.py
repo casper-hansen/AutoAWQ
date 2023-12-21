ed9a0d75b3dfab68f27e89700d5d847b7824f20b
Casper Hansen <casperbh.96@gmail.com>
2023-12-21 22:11:14 +0000

    Update examples

--- examples/basic_quant.py
+++ examples/basic_quant.py
@@ -1,21 +1,19 @@
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 
-model_path = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
-quant_path = 'mixtral-instruct-awq'
-modules_to_not_convert = ["gate"]
-quant_config = {
-    "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM",
-    "modules_to_not_convert": modules_to_not_convert
-}
+model_path = 'lmsys/vicuna-7b-v1.5'
+quant_path = 'vicuna-7b-v1.5-awq'
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
 
 # Load model
 # NOTE: pass safetensors=True to load safetensors
-model = AutoAWQForCausalLM.from_pretrained(model_path, safetensors=True, **{"low_cpu_mem_usage": True})
+model = AutoAWQForCausalLM.from_pretrained(
+    model_path, **{"low_cpu_mem_usage": True, "use_cache": False}
+)
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 # Quantize
-model.quantize(tokenizer, quant_config=quant_config, modules_to_not_convert=modules_to_not_convert)
+model.quantize(tokenizer, quant_config=quant_config)
 
 # Save quantized model
 model.save_quantized(quant_path)
