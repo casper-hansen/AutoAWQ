b02263f6373a8c2f6156005aecb7c4d620451a9c
Casper Hansen <casperbh.96@gmail.com>
2024-01-01 17:31:28 +0000

    Update example

--- examples/awq_to_gguf_quant.py
+++ examples/awq_to_gguf_quant.py
@@ -4,9 +4,9 @@ from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 
 model_path = 'TheBloke/Llama-2-7B-Chat-fp16'
-quant_path = 'llama-2-7b-3bit-awq'
+quant_path = 'llama-2-7b-chat-awq'
 llama_cpp_path = '/workspace/llama.cpp'
-quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 3, "version": "GEMM" }
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
 
 # Load model
 # NOTE: pass safetensors=True to load safetensors
@@ -31,32 +31,18 @@ print(f'Model is quantized and saved at "{quant_path}"')
 
 # GGUF conversion
 print('Converting model to GGUF...')
+llama_cpp_method = f"q{quant_config['w_bit']}_0"
+convert_cmd_path = os.path.join(llama_cpp_path, "convert.py")
+quantize_cmd_path = os.path.join(llama_cpp_path, "quantize")
 
 if not os.path.exists(llama_cpp_path):
-    cmd = f"git clone https://github.com/ggerganov/llama.cpp.git {llama_cpp_path} && cd {llama_cpp_path} && make LLAMA_CUBLAS=1"
+    cmd = f"git clone https://github.com/ggerganov/llama.cpp.git {llama_cpp_path} && cd {llama_cpp_path} && make LLAMA_CUBLAS=1 LLAMA_CUDA_F16=1"
     subprocess.run([cmd], shell=True, check=True)
 
-# Step 1: python convert.py {quant_path} --outfile {quant_path}/model.gguf
-convert_script = "convert.py" if model.model_type == 'llama' else "convert-hf-to-gguf.py"
-convert_cmd_path = os.path.join(llama_cpp_path, convert_script)
 subprocess.run([
     f"python {convert_cmd_path} {quant_path} --outfile {quant_path}/model.gguf"
 ], shell=True, check=True)
 
-# Step 2: ./quantize {quant_path}/model.gguf {quant_path}/model_q3.gguf q3_k
-llama_cpp_method = f"q{quant_config['w_bit']}_k"
-quantize_cmd_path = os.path.join(llama_cpp_path, "quantize")
 subprocess.run([
     f"{quantize_cmd_path} {quant_path}/model.gguf {quant_path}/model_{llama_cpp_method}.gguf {llama_cpp_method}"
 ], shell=True, check=True)
-
-# Step 3: Inference using llama.cpp
-main_cmd_path = os.path.join(llama_cpp_path, "main")
-prompt = "[INST] <<SYS>>You are a helpful, respectful and honest assistant.<</SYS>>Hello![/INST]"
-cmd = f"""{main_cmd_path} \
-  -m {quant_path}/model_{llama_cpp_method}.gguf \
-  -p "{prompt}" \
-  --repeat_penalty 1 \
-  --no-penalize-nl \
-  --color --temp 0 -c 512 --n-gpu-layers 33"""
-subprocess.run([cmd], shell=True, check=True)
