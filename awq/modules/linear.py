45ae7dfd83a9de1ed0f6639b89382405da6c529b
ilyas@huggingface.co <moutawwakil.ilyas.tsi@gmail.com>
2024-01-19 07:46:57 +0000

    added exllama wqlinear layer

--- awq/modules/linear.py
+++ awq/modules/linear.py
@@ -1,11 +1,43 @@
 import math
 import torch
 import torch.nn as nn
+
+# this will raise an error for AMD GPUs for example
 import awq_ext  # with CUDA kernels
+from awq.utils.exllama_utils import unpack_awq, pack_from_tensors
+
+try:
+    # this requires installing auto_gptq correctly (for AMD GPUs for example)
+    from exllama_kernels import make_q4, q4_matmul
+except ImportError:
+    make_q4 = None
+    q4_matmul = None
+
+# Dummy tensor to pass instead of g_idx since there is no way to pass "None" to a C++ extension
+none_tensor = torch.empty((1, 1), device="meta")
+
+
+def ext_make_q4(qweight, qzeros, scales, g_idx, device):
+    """Construct Q4Matrix, return handle"""
+    return make_q4(
+        qweight, qzeros, scales, g_idx if g_idx is not None else none_tensor, device
+    )
+
+
+def ext_q4_matmul(x, q4, q4_width):
+    """Matrix multiplication, returns x @ q4"""
+    outshape = x.shape[:-1] + (q4_width,)
+    x = x.view(-1, x.shape[-1])
+    output = torch.empty((x.shape[0], q4_width), dtype=torch.float16, device=x.device)
+
+    q4_matmul(x, q4, output)
+
+    return output.view(outshape)
 
 
 def make_divisible(c, divisor):
     return (c + divisor - 1) // divisor
+
 
 def calculate_zeros_width(in_features, group_size=128, pack_num=8):
     if group_size >= 128:
@@ -16,59 +48,101 @@ def calculate_zeros_width(in_features, group_size=128, pack_num=8):
         size_multiplier = 4
     else:
         raise NotImplementedError
-    
+
     base_width = make_divisible(in_features // group_size, pack_num)
     base_width = make_divisible(base_width, size_multiplier) * size_multiplier
     return base_width
 
+
 class WQLinear_GEMM(nn.Module):
     def __init__(self, w_bit, group_size, in_features, out_features, bias, dev):
         super().__init__()
-        
+
         if w_bit not in [4]:
             raise NotImplementedError("Only 4-bit are supported for now.")
-        
+
         self.in_features = in_features
         self.out_features = out_features
         self.w_bit = w_bit
         self.group_size = group_size if group_size != -1 else in_features
-        
+
         # quick sanity check (make sure aligment)
         assert self.in_features % self.group_size == 0
         assert out_features % (32 // self.w_bit) == 0
 
-        self.register_buffer('qweight', torch.zeros((in_features, out_features // (32 // self.w_bit)), dtype=torch.int32, device=dev))
-        self.register_buffer('qzeros', torch.zeros((in_features // self.group_size, out_features // (32 // self.w_bit)), dtype=torch.int32, device=dev))
-        self.register_buffer('scales', torch.zeros((in_features // self.group_size, out_features), dtype=torch.float16, device=dev))
+        self.register_buffer(
+            "qweight",
+            torch.zeros(
+                (in_features, out_features // (32 // self.w_bit)),
+                dtype=torch.int32,
+                device=dev,
+            ),
+        )
+        self.register_buffer(
+            "qzeros",
+            torch.zeros(
+                (in_features // self.group_size, out_features // (32 // self.w_bit)),
+                dtype=torch.int32,
+                device=dev,
+            ),
+        )
+        self.register_buffer(
+            "scales",
+            torch.zeros(
+                (in_features // self.group_size, out_features),
+                dtype=torch.float16,
+                device=dev,
+            ),
+        )
         if bias:
-            self.register_buffer('bias', torch.zeros((out_features), dtype=torch.float16, device=dev))
+            self.register_buffer(
+                "bias", torch.zeros((out_features), dtype=torch.float16, device=dev)
+            )
         else:
             self.bias = None
 
     @classmethod
-    def from_linear(cls, linear, w_bit, group_size, init_only=False, scales=None, zeros=None):
-        awq_linear = cls(w_bit, group_size, linear.in_features, linear.out_features, linear.bias is not None, linear.weight.device)
+    def from_linear(
+        cls, linear, w_bit, group_size, init_only=False, scales=None, zeros=None
+    ):
+        awq_linear = cls(
+            w_bit,
+            group_size,
+            linear.in_features,
+            linear.out_features,
+            linear.bias is not None,
+            linear.weight.device,
+        )
         if init_only:  # just prepare for loading sd
             return awq_linear
-        
+
         # need scales and zeros info for real quantization
-        assert scales is not None and zeros is not None  
+        assert scales is not None and zeros is not None
         scale_zeros = zeros * scales
-        
+
         awq_linear.scales = scales.clone().half()
         if linear.bias is not None:
             awq_linear.bias = linear.bias.clone().half()
 
         pack_num = 32 // awq_linear.w_bit
-        
+
         intweight = []
         for idx in range(awq_linear.in_features):
-            intweight.append(torch.round((linear.weight.data[:, idx] + scale_zeros[idx // group_size]) / awq_linear.scales[idx // group_size]).to(torch.int)[:, None])
+            intweight.append(
+                torch.round(
+                    (linear.weight.data[:, idx] + scale_zeros[idx // group_size])
+                    / awq_linear.scales[idx // group_size]
+                ).to(torch.int)[:, None]
+            )
         intweight = torch.cat(intweight, dim=1)
         intweight = intweight.t().contiguous()
         intweight = intweight.to(dtype=torch.int32)
-        qweight = torch.zeros((intweight.shape[0], intweight.shape[1] // 32 * awq_linear.w_bit), dtype=torch.int32, device=intweight.device)           
-         
+        qweight = torch.zeros(
+            (intweight.shape[0], intweight.shape[1] // 32 * awq_linear.w_bit),
+            dtype=torch.int32,
+            device=intweight.device,
+        )
+
         for col in range(intweight.shape[1] // pack_num):
             if awq_linear.w_bit == 4:
                 order_map = [0, 2, 4, 6, 1, 3, 5, 7]
@@ -80,8 +154,12 @@ class WQLinear_GEMM(nn.Module):
         awq_linear.qweight = qweight
 
         zeros = zeros.to(dtype=torch.int32)
-        qzeros = torch.zeros((zeros.shape[0], zeros.shape[1] // 32 * awq_linear.w_bit), dtype=torch.int32, device=zeros.device)
-        
+        qzeros = torch.zeros(
+            (zeros.shape[0], zeros.shape[1] // 32 * awq_linear.w_bit),
+            dtype=torch.int32,
+            device=zeros.device,
+        )
+
         for col in range(zeros.shape[1] // pack_num):
             if awq_linear.w_bit == 4:
                 order_map = [0, 2, 4, 6, 1, 3, 5, 7]
@@ -91,38 +169,46 @@ class WQLinear_GEMM(nn.Module):
                 qzero_col = zeros[:, col * pack_num + order_map[i]]
                 qzeros[:, col] |= qzero_col << (i * awq_linear.w_bit)
         awq_linear.qzeros = qzeros
-        
+
         return awq_linear
 
     @torch.no_grad()
     def forward(self, x):
-        out_shape = x.shape[:-1] + (self.out_features, )
+        out_shape = x.shape[:-1] + (self.out_features,)
 
         input_dtype = x.dtype
         if input_dtype != torch.float16:
             x = x.half()
-        
-        out = awq_ext.gemm_forward_cuda(x.reshape(-1, x.shape[-1]), self.qweight, self.scales, self.qzeros, 8)
-        
+
+        out = awq_ext.gemm_forward_cuda(
+            x.reshape(-1, x.shape[-1]), self.qweight, self.scales, self.qzeros, 8
+        )
+
         if input_dtype != torch.float16:
             out = out.to(dtype=input_dtype)
-        
+
         out = out + self.bias if self.bias is not None else out
         return out.reshape(out_shape)
-    
+
     def extra_repr(self) -> str:
-        return 'in_features={}, out_features={}, bias={}, w_bit={}, group_size={}'.format(
-            self.in_features, self.out_features, self.bias is not None, self.w_bit, self.group_size
+        return (
+            "in_features={}, out_features={}, bias={}, w_bit={}, group_size={}".format(
+                self.in_features,
+                self.out_features,
+                self.bias is not None,
+                self.w_bit,
+                self.group_size,
+            )
         )
 
 
 class WQLinear_GEMV(nn.Module):
     def __init__(self, w_bit, group_size, in_features, out_features, bias, dev):
         super().__init__()
-        
+
         if w_bit not in [4]:
             raise NotImplementedError("Only 4-bit are supported for now.")
-        
+
         self.in_features = in_features
         self.out_features = out_features
         self.w_bit = w_bit
@@ -132,44 +218,89 @@ class WQLinear_GEMV(nn.Module):
         # quick sanity check (make sure aligment)
         assert self.in_features % self.group_size == 0
         assert out_features % (32 // self.w_bit) == 0
-        pack_num = (32 // self.w_bit)
+        pack_num = 32 // self.w_bit
 
-        self.register_buffer('qweight', torch.zeros((out_features, in_features // pack_num), dtype=torch.int32, device=dev))
-        self.register_buffer('qzeros', torch.zeros((out_features, calculate_zeros_width(in_features, self.group_size)), dtype=torch.int32, device=dev))
-        self.register_buffer('scales', torch.zeros((out_features, calculate_zeros_width(in_features, self.group_size) * pack_num), dtype=torch.float16, device=dev))
+        self.register_buffer(
+            "qweight",
+            torch.zeros(
+                (out_features, in_features // pack_num), dtype=torch.int32, device=dev
+            ),
+        )
+        self.register_buffer(
+            "qzeros",
+            torch.zeros(
+                (out_features, calculate_zeros_width(in_features, self.group_size)),
+                dtype=torch.int32,
+                device=dev,
+            ),
+        )
+        self.register_buffer(
+            "scales",
+            torch.zeros(
+                (
+                    out_features,
+                    calculate_zeros_width(in_features, self.group_size) * pack_num,
+                ),
+                dtype=torch.float16,
+                device=dev,
+            ),
+        )
         if bias:
-            self.register_buffer('bias', torch.zeros((out_features), dtype=torch.float16, device=dev))
+            self.register_buffer(
+                "bias", torch.zeros((out_features), dtype=torch.float16, device=dev)
+            )
         else:
             self.bias = None
 
     @classmethod
-    def from_linear(cls, linear, w_bit, group_size, init_only=False, scales=None, zeros=None):
-        awq_linear = cls(w_bit, group_size, linear.in_features, linear.out_features, linear.bias is not None, linear.weight.device)
+    def from_linear(
+        cls, linear, w_bit, group_size, init_only=False, scales=None, zeros=None
+    ):
+        awq_linear = cls(
+            w_bit,
+            group_size,
+            linear.in_features,
+            linear.out_features,
+            linear.bias is not None,
+            linear.weight.device,
+        )
         if init_only:  # just prepare for loading sd
             return awq_linear
-        
+
         # need scales and zeros info for real quantization
-        assert scales is not None and zeros is not None  
+        assert scales is not None and zeros is not None
         scale_zeros = zeros * scales
 
         pack_num = 32 // awq_linear.w_bit
         qscales = torch.zeros(
-            (scales.shape[0], calculate_zeros_width(linear.in_features, group_size) * pack_num),
+            (
+                scales.shape[0],
+                calculate_zeros_width(linear.in_features, group_size) * pack_num,
+            ),
             dtype=torch.float16,
-            device=scales.device
+            device=scales.device,
         )
-        qscales[:, :scales.shape[1]] = scales
+        qscales[:, : scales.shape[1]] = scales
         awq_linear.scales = qscales
         if linear.bias is not None:
             awq_linear.bias = linear.bias.clone().half()
-        
+
         intweight = []
         for idx in range(awq_linear.in_features):
-            intweight.append(torch.round((linear.weight.data[:, idx] + scale_zeros[:, idx // group_size]) / awq_linear.scales[:, idx // group_size]).to(torch.int)[:, None])
+            intweight.append(
+                torch.round(
+                    (linear.weight.data[:, idx] + scale_zeros[:, idx // group_size])
+                    / awq_linear.scales[:, idx // group_size]
+                ).to(torch.int)[:, None]
+            )
         intweight = torch.cat(intweight, dim=1)
         intweight = intweight.to(dtype=torch.int32)
-        qweight = torch.zeros((intweight.shape[0], intweight.shape[1] // 32 * awq_linear.w_bit), dtype=torch.int32, device=intweight.device)           
-         
+        qweight = torch.zeros(
+            (intweight.shape[0], intweight.shape[1] // 32 * awq_linear.w_bit),
+            dtype=torch.int32,
+            device=intweight.device,
+        )
+
         for col in range(intweight.shape[1] // pack_num):
             if awq_linear.w_bit == 4:
                 order_map = [0, 1, 2, 3, 4, 5, 6, 7]
@@ -186,7 +317,7 @@ class WQLinear_GEMV(nn.Module):
             dtype=torch.int32,
             device=zeros.device,
         )
-        
+
         for col in range((zeros.shape[1] + pack_num - 1) // pack_num):
             if awq_linear.w_bit == 4:
                 order_map = [0, 1, 2, 3, 4, 5, 6, 7]
@@ -202,25 +333,143 @@ class WQLinear_GEMV(nn.Module):
 
     @torch.no_grad()
     def forward(self, x):
-        out_shape = x.shape[:-1] + (self.out_features, )
+        out_shape = x.shape[:-1] + (self.out_features,)
         inputs = x.reshape(-1, x.shape[-1])
 
         input_dtype = inputs.dtype
         if input_dtype != torch.float16:
             inputs = inputs.half()
-        
+
         if inputs.shape[0] > 8:
-            out = awq_ext.gemmv2_forward_cuda(inputs, self.qweight, self.scales, self.qzeros, self.group_size, self.split_k_iters)
+            out = awq_ext.gemmv2_forward_cuda(
+                inputs,
+                self.qweight,
+                self.scales,
+                self.qzeros,
+                self.group_size,
+                self.split_k_iters,
+            )
         else:
-            out = awq_ext.gemv_forward_cuda(inputs, self.qweight, self.scales, self.qzeros, self.group_size)
+            out = awq_ext.gemv_forward_cuda(
+                inputs, self.qweight, self.scales, self.qzeros, self.group_size
+            )
 
         if input_dtype != torch.float16:
             out = out.to(dtype=input_dtype)
-        
+
         out = out + self.bias if self.bias is not None else out
         return out.reshape(out_shape)
-    
+
     def extra_repr(self) -> str:
-        return 'in_features={}, out_features={}, bias={}, w_bit={}, group_size={}'.format(
-            self.in_features, self.out_features, self.bias is not None, self.w_bit, self.group_size
+        return (
+            "in_features={}, out_features={}, bias={}, w_bit={}, group_size={}".format(
+                self.in_features,
+                self.out_features,
+                self.bias is not None,
+                self.w_bit,
+                self.group_size,
+            )
         )
+
+
+class WQLinear_Exllama(nn.Module):
+    def __init__(self, w_bit, group_size, in_features, out_features, bias, dev):
+        super().__init__()
+
+        if w_bit not in [4]:
+            raise NotImplementedError("Only 4-bit are supported for now.")
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.w_bit = w_bit
+        self.group_size = group_size if group_size != -1 else in_features
+
+        self.register_buffer(
+            "qweight",
+            torch.zeros(
+                (in_features // 32 * w_bit), out_features, dtype=torch.int32, device=dev
+            ),
+        )
+        self.register_buffer(
+            "qzeros",
+            torch.zeros(
+                (
+                    math.ceil(in_features / group_size),
+                    out_features // 32 * w_bit,
+                ),
+                dtype=torch.int32,
+                device=dev,
+            ),
+        )
+        self.register_buffer(
+            "scales",
+            torch.zeros(
+                (math.ceil(in_features / group_size), out_features),
+                dtype=torch.float16,
+                device=dev,
+            ),
+        )
+
+        if bias:
+            self.register_buffer(
+                "bias", torch.zeros((out_features), dtype=torch.float16, device=dev)
+            )
+        else:
+            self.bias = None
+
+    def post_init(self):
+        assert self.qweight.device.type == "cuda"
+        assert self.qweight.device.index is not None
+
+        self.q4 = ext_make_q4(
+            self.qweight,
+            self.qzeros,
+            self.scales,
+            None,
+            self.qweight.device.index,  # device index
+        )
+
+    @classmethod
+    def from_wqlinear_gemm(cls, q_linear):
+        # Create a new instance of the WQLinear class from ExllamaLinear with the same parameters
+        exllama_linear = WQLinear_Exllama(
+            w_bit=q_linear.w_bit,
+            group_size=q_linear.group_size,
+            in_features=q_linear.in_features,
+            out_features=q_linear.out_features,
+            bias=q_linear.bias,
+            dev=q_linear.qweight.device,
+        )
+
+        # Unpack the qweight and qzeros tensors
+        fp16_weight, zeros = unpack_awq(
+            q_linear.qweight,
+            q_linear.qzeros,
+            q_linear.scales,
+            q_linear.w_bit,
+            q_linear.group_size,
+        )
+
+        # Pack the qweight and qzeros tensors
+        qweight, qzeros = pack_from_tensors(
+            fp16_weight,
+            zeros,
+            q_linear.scales,
+            q_linear.w_bit,
+            q_linear.group_size,
+        )
+
+        # Copy the packed tensors to the ExllamaLinear instance
+        exllama_linear.qweight.copy_(qweight)
+        exllama_linear.qzeros.copy_(qzeros)
+        exllama_linear.scales.copy_(q_linear.scales)
+
+        return exllama_linear
+
+    def forward(self, x):
+        out = ext_q4_matmul(x.half(), self.q4, self.out_features)
+
+        if self.bias is not None:
+            out.add_(self.bias)
+
+        return out
