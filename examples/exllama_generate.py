2c3d6c3b6e9a7a1b1b4e16f86e212afd5ad952a7
ilyas@huggingface.co <moutawwakil.ilyas.tsi@gmail.com>
2024-01-19 07:48:20 +0000

    added example

---
+++ examples/exllama_generate.py
@@ -0,0 +1,27 @@
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
+quant_path = "TheBloke/TinyLlama-1.1B-python-v0.1-AWQ"
+
+# Load model
+tokenizer = AutoTokenizer.from_pretrained(quant_path)
+# Generate output
+inp = tokenizer("def hello_", return_tensors="pt").input_ids.to("cuda")
+
+# Load model
+print("Loading GEMM model...")
+gemm_model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=False)
+gemm_out = gemm_model.generate(inp, max_new_tokens=100)
+
+print("GEMM output:")
+print(tokenizer.decode(gemm_out[0]))
+print()
+
+print("Loading Exllama model...")
+exllama_model = AutoAWQForCausalLM.from_quantized(
+    quant_path, fuse_layers=False, use_exllama=True
+)
+exllama_out = exllama_model.generate(inp, max_new_tokens=100)
+
+print("Exllama output:")
+print(tokenizer.decode(exllama_out[0]))
