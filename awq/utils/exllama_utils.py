ac374de6e61b3055047bb244edce4556a34e7767
ilyas@huggingface.co <moutawwakil.ilyas.tsi@gmail.com>
2024-01-21 07:11:47 +0000

    more optimized packing/unpacking

--- awq/utils/exllama_utils.py
+++ awq/utils/exllama_utils.py
@@ -1,160 +1,80 @@
 import torch
 
 
-# non tensor for exllama kernels
-none_tensor = torch.empty((1, 1), device="meta")
+AWQ_ORDER = [0, 2, 4, 6, 1, 3, 5, 7]
+AWQ_REVERSE_ORDER = [0, 4, 1, 5, 2, 6, 3, 7]
 
 
-def awq_reverse_reorder_int_tensor(int_tensor, bits: int):
-    compress_ratio = 32 // bits
-    order_map = [0, 2, 4, 6, 1, 3, 5, 7]
-
-    assert int_tensor.shape[-1] % compress_ratio == 0
-
-    order_tensor = torch.tensor(
-        order_map,
-        dtype=torch.int32,
-        device=int_tensor.device,
-    )[None, :]
-
-    order_tensor = order_tensor.repeat(int_tensor.shape[1] // compress_ratio, 1)
-
-    order_tensor += torch.arange(
-        0,
-        int_tensor.shape[-1],
-        compress_ratio,
-        dtype=torch.int32,
-        device=int_tensor.device,
-    )[:, None]
-
-    order_tensor = order_tensor.view(-1)
-
-    reverse_order_tensor = torch.arange(
-        order_tensor.shape[0],
-        dtype=torch.int32,
-        device=int_tensor.device,
-    )[order_tensor][order_tensor]
-
-    int_tensor = int_tensor[:, reverse_order_tensor]
-
-    return int_tensor
-
-
-def unpack(qweight: torch.Tensor, qzeros: torch.Tensor, bits: int):
-    qweight = qweight.T
+def unpack_awq(qweight: torch.Tensor, qzeros: torch.Tensor, bits: int):
     shifts = torch.arange(0, 32, bits, device=qzeros.device)
 
-    izeros = torch.bitwise_right_shift(qzeros[:, :, None], shifts[None, None, :])
-    izeros = izeros.reshape(-1, izeros.shape[1] * izeros.shape[2])
+    # unpacking columnwise
+    iweights = torch.bitwise_right_shift(qweight[:, :, None], shifts[None, None, :]).to(
+        torch.int8  # smallest dtype available
+    )
+    iweights = iweights.view(iweights.shape[0], -1)
 
-    iweights = torch.bitwise_right_shift(qweight[:, None, :], shifts[None, :, None])
-    iweights = torch.bitwise_and(iweights, (2**bits) - 1)
-    iweights = iweights.reshape(-1, iweights.shape[-1])
+    # unpacking columnwise
+    izeros = torch.bitwise_right_shift(qzeros[:, :, None], shifts[None, None, :]).to(
+        torch.int8  # smallest dtype available
+    )
+    izeros = izeros.view(izeros.shape[0], -1)
 
     return iweights, izeros
 
 
-def awq_reverse_reorder(iweights: torch.Tensor, izeros: torch.Tensor, bits: int):
-    iweights = iweights.T
+def reverse_awq_order(iweights: torch.Tensor, izeros: torch.Tensor, bits: int):
+    reverse_order_tensor = torch.arange(
+        izeros.shape[-1],
+        dtype=torch.int32,
+        device=izeros.device,
+    )
+    reverse_order_tensor = reverse_order_tensor.view(-1, 32 // bits)
+    reverse_order_tensor = reverse_order_tensor[:, AWQ_REVERSE_ORDER]
+    reverse_order_tensor = reverse_order_tensor.view(-1)
 
-    izeros = awq_reverse_reorder_int_tensor(izeros, bits)
-    iweights = awq_reverse_reorder_int_tensor(iweights, bits)
+    izeros = izeros[:, reverse_order_tensor]
+    iweights = iweights[:, reverse_order_tensor]
 
     return iweights, izeros
 
 
-def pack(iweights: torch.Tensor, izeros: torch.Tensor, bits: int):
-    qweight = torch.zeros(
-        (iweights.shape[0] // 32 * bits, iweights.shape[1]),
-        dtype=torch.int32,
-        device=iweights.device,
+def pack_exllama(iweights: torch.Tensor, izeros: torch.Tensor, bits: int):
+    shifts = torch.arange(0, 32, bits, device=iweights.device)
+
+    # packing rowwise
+    iweights = iweights.view(iweights.shape[0] // (32 // bits), 32 // bits, -1)
+    qweight = (
+        torch.bitwise_left_shift(iweights, shifts[None, :, None])
+        .sum(dim=1)
+        .to(torch.int32)
     )
-    rows = torch.arange(
-        qweight.shape[0],
-        dtype=torch.int32,
-        device=iweights.device,
-    ) * (32 // bits)
 
-    for j in range(32 // bits):
-        qweight = torch.bitwise_or(
-            qweight, torch.bitwise_left_shift(iweights[j + rows], (bits * j))
-        )
-
-    qzeros = torch.zeros(
-        (izeros.shape[0], izeros.shape[1] // 32 * bits),
-        dtype=torch.int32,
-        device=izeros.device,
+    # packing columnwise
+    izeros = izeros.view(-1, izeros.shape[1] // (32 // bits), 32 // bits)
+    qzeros = (
+        torch.bitwise_left_shift(izeros, shifts[None, None, :])
+        .sum(dim=-1)
+        .to(torch.int32)
     )
-    cols = torch.arange(
-        qzeros.shape[1],
-        dtype=torch.int32,
-        device=izeros.device,
-    ) * (32 // bits)
-
-    for j in range(32 // bits):
-        qzeros = torch.bitwise_or(
-            qzeros,
-            torch.bitwise_left_shift(izeros[:, j + cols], (bits * j)),
-        )
 
     return qweight, qzeros
 
 
-def exllama_post_init(model):
-    for _, submodule in model.named_modules():
-        if hasattr(submodule, "QUANT_TYPE") and submodule.QUANT_TYPE == "exllama":
-            submodule.post_init()
+def unpack_reorder_pack(qweight, qzeros, bits):
+    # Unpack the qweight and qzeros tensors
+    iweight, izeros = unpack_awq(qweight, qzeros, bits)
+    # Reverse the order of the iweight and izeros tensors
+    iweight, izeros = reverse_awq_order(iweight, izeros, bits)
 
-    return model
+    # overflow checks
+    iweight = torch.bitwise_and(iweight, (2**bits) - 1)
+    izeros = torch.bitwise_and(izeros, (2**bits) - 1)
 
+    # Subtract 1 from the izeros tensor (exllama adds 1 during inference)
+    # We can remove it if we remove the +1 in the exllama code
+    izeros = izeros - 1
+    # Pack the qweight and qzeros tensors
+    qweight, qzeros = pack_exllama(iweight, izeros, bits)
 
-def exllamav2_post_init(model):
-    fixed_bytes = {}
-    for _, submodule in model.named_modules():
-        if hasattr(submodule, "QUANT_TYPE") and submodule.QUANT_TYPE == "exllamav2":
-            device = submodule.qweight.device
-            if device not in fixed_bytes:
-                fixed_bytes[device] = 0
-
-            scratch_fixed = submodule.scratch_space_fixed()
-            fixed_bytes[device] = max(fixed_bytes[device], scratch_fixed)
-
-    device_tensors = {}
-    for device, scratch_bytes in fixed_bytes.items():
-        device_tensors[device] = ExLlamaV2DeviceTensors(device, scratch_bytes)
-
-    # have persistent buffers, otherwise we will get OOM
-    model.device_tensors = device_tensors
-
-    for _, submodule in model.named_modules():
-        if hasattr(submodule, "QUANT_TYPE") and submodule.QUANT_TYPE == "exllamav2":
-            device = submodule.qweight.device
-            submodule.post_init(temp_dq=model.device_tensors[device])
-
-    return model
-
-
-class ExLlamaV2DeviceTensors:
-    dev: torch.device
-    scratch_bytes: int
-    scratch: torch.tensor = None
-
-    def __init__(self, dev, scratch_bytes):
-        self.dev = dev
-        self.scratch_bytes = scratch_bytes
-
-    def prepare(self):
-        self.scratch = torch.empty(
-            self.scratch_bytes // 2,
-            dtype=torch.float16,
-            device=self.dev,
-        )
-
-    def get_scratch_slice(self, size_bytes):
-        if self.scratch is None:
-            self.prepare()
-
-        size_bytes = ((size_bytes + 127) // 128) * 128
-        size_half = size_bytes // 2
-        scratch_slice = self.scratch.narrow(0, 0, size_half)
-        return scratch_slice
+    return qweight, qzeros
--- examples/exllama_generate.py
+++ examples/exllama_generate.py
@@ -1,38 +1,46 @@
+import torch
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
 
-quant_path = "TheBloke/TinyLlama-1.1B-python-v0.1-AWQ"
-
+quant_path = "TheBloke/Mistral-7B-Instruct-v0.1-AWQ"
 tokenizer = AutoTokenizer.from_pretrained(quant_path)
 
+
+def report_diff(awq, other):
+    reldiff = (awq - other).abs() / (awq.abs() + 1e-15)
+    print("p90 reldiff with AWQ", torch.quantile(reldiff, 0.9))
+    print("Median reldiff with AWQ", reldiff.median())
+    print("Mean reldiff with AWQ", reldiff.mean())
+
+
+print("Model: ", quant_path)
 print("Loading GEMM model...")
 inp = tokenizer("def hello_", return_tensors="pt").input_ids.to("cuda")
 gemm_model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=False)
-
 gemm_logits = gemm_model(inp).logits
-gemm_ids = gemm_model.generate(inp, max_new_tokens=20)
+gemm_ids = gemm_model.generate(inp, max_new_tokens=50)
 
-print("GEMM output:")
-print("=====================================")
-print(tokenizer.decode(gemm_ids[0]))
-print("=====================================")
+
+# print("GEMM output:")
+# print("=====================================")
+# print(tokenizer.decode(gemm_ids[0]))
+# print("=====================================")
+
 
 print("Loading Exllama model...")
 inp = tokenizer("def hello_", return_tensors="pt").input_ids.to("cuda")
 exllama_model = AutoAWQForCausalLM.from_quantized(
     quant_path, fuse_layers=False, use_exllama=True
 )
-
 exllama_logits = exllama_model(inp).logits
-print("Max difference with GEMM:", (gemm_logits - exllama_logits).abs().max())
-print("Mean difference with GEMM:", (gemm_logits - exllama_logits).abs().mean())
+report_diff(gemm_logits, exllama_logits)
 
 
-print("Exllama output:")
-print("=====================================")
-exllama_ids = exllama_model.generate(inp, max_new_tokens=20)
-print(tokenizer.decode(exllama_ids[0]))
-print("=====================================")
+# print("Exllama output:")
+# print("=====================================")
+# exllama_ids = exllama_model.generate(inp, max_new_tokens=50)
+# print(tokenizer.decode(exllama_ids[0]))
+# print("=====================================")
 
 
 print("Loading ExllamaV2 model...")
@@ -40,13 +48,12 @@ inp = tokenizer("def hello_", return_tensors="pt").input_ids.to("cuda")
 exllama_v2_model = AutoAWQForCausalLM.from_quantized(
     quant_path, fuse_layers=False, use_exllama_v2=True
 )
-
 exllama_v2_logits = exllama_v2_model(inp).logits
-print("Max difference with GEMM:", (gemm_logits - exllama_v2_logits).abs().max())
-print("Mean difference with GEMM:", (gemm_logits - exllama_v2_logits).abs().mean())
+report_diff(gemm_logits, exllama_v2_logits)
 
-print("ExllamaV2 output:")
-print("=====================================")
-exllama_v2_ids = exllama_v2_model.generate(inp, max_new_tokens=20)
-print(tokenizer.decode(exllama_v2_ids[0]))
-print("=====================================")
+
+# print("ExllamaV2 output:")
+# print("=====================================")
+# exllama_v2_ids = exllama_v2_model.generate(inp, max_new_tokens=50)
+# print(tokenizer.decode(exllama_v2_ids[0]))
+# print("=====================================")
