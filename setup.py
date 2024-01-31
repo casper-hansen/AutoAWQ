4faa3bd18145cd95c725319ec1dcae1fc06d27d8
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2024-01-31 12:36:14 +0000

    fix

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 import torch
 import platform
 import requests
+import importlib.util
 from pathlib import Path
 from setuptools import setup, find_packages
 
@@ -12,7 +13,9 @@ def get_latest_kernels_version(repo):
     """
     response = requests.get(f"https://api.github.com/repos/{repo}/releases/latest")
     data = response.json()
-    return data["tag_name"]
+    tag_name = data["tag_name"]
+    version = tag_name.replace("v", "")
+    return version
 
 
 def get_kernels_whl_url(
@@ -25,27 +28,36 @@ def get_kernels_whl_url(
     """
     Get the url for the kernels wheel file.
     """
-    return f"https://github.com/casper-hansen/AutoAWQ_kernels/releases/download/{release_version}/autoawq_kernels-{release_version[1:]}+{gpu_system_version}-{python_version}-{python_version}-{platform}_{architecture}.whl"
+    return f"https://github.com/casper-hansen/AutoAWQ_kernels/releases/download/v{release_version}/autoawq_kernels-{release_version}+{gpu_system_version}-cp{python_version}-cp{python_version}-{platform}_{architecture}.whl"
 
 
 AUTOAWQ_VERSION = "0.1.8"
 PYPI_BUILD = os.getenv("PYPI_BUILD", "0") == "1"
-CUDA_VERSION = os.getenv("CUDA_VERSION", None) or torch.version.cuda
-ROCM_VERSION = os.getenv("ROCM_VERSION", None) or torch.version.hip
 
-if isinstance(ROCM_VERSION, str):
+try:
+    importlib.metadata.version("autoawq-kernels")
+    KERNELS_INSTALLED = True
+except importlib.metadata.PackageNotFoundError:
+    KERNELS_INSTALLED = False
+
+CUDA_VERSION = os.getenv("CUDA_VERSION", None) or torch.version.cuda
+if CUDA_VERSION:
+    CUDA_VERSION = "".join(CUDA_VERSION.split("."))[:3]
+
+ROCM_VERSION = os.getenv("ROCM_VERSION", None) or torch.version.hip
+if ROCM_VERSION:
     if ROCM_VERSION.startswith("5.6"):
         ROCM_VERSION = "5.6.1"
     elif ROCM_VERSION.startswith("5.7"):
         ROCM_VERSION = "5.7.1"
 
+    ROCM_VERSION = "".join(ROCM_VERSION.split("."))[:3]
+
 
 if not PYPI_BUILD:
     if CUDA_VERSION:
-        CUDA_VERSION = "".join(CUDA_VERSION.split("."))[:3]
         AUTOAWQ_VERSION += f"+cu{CUDA_VERSION}"
     elif ROCM_VERSION:
-        ROCM_VERSION = "".join(ROCM_VERSION.split("."))[:3]
         AUTOAWQ_VERSION += f"+rocm{ROCM_VERSION}"
     else:
         raise RuntimeError(
@@ -87,23 +99,31 @@ requirements = [
     "datasets",
 ]
 
-
-# kernels can be downloaded from pypi for cuda+(linux or windows)
-if platform.system().lower() != "darwin" and CUDA_VERSION:
-    requirements.append("autoawq-kernels")
-elif platform.system().lower() != "darwin" and ROCM_VERSION:
-    kernels_version = get_latest_kernels_version("casper-hansen/AutoAWQ_kernels")
-    python_version = ".".join(platform.python_version_tuple()[:2])
-    platform_name = platform.system().lower()
-    architecture = platform.machine().lower()
-    latest_rocm_kernels_wheels = get_kernels_whl_url(
-        f"rocm{ROCM_VERSION}",
-        kernels_version,
-        python_version,
-        platform_name,
-        architecture,
-    )
-    requirements.append(latest_rocm_kernels_wheels)
+# kernels can be downloaded from pypi for cuda+121+(linux or windows) only
+# for everything else, we need to download the kernels wheel from github
+if platform.system().lower() != "darwin" and not KERNELS_INSTALLED:
+    if CUDA_VERSION.startswith("12"):
+        requirements.append("autoawq-kernels")
+    elif CUDA_VERSION.startswith("11") or ROCM_VERSION in ["561", "571"]:
+        gpu_system_version = (
+            f"cu{CUDA_VERSION}" if CUDA_VERSION else f"rocm{ROCM_VERSION}"
+        )
+        kernels_version = get_latest_kernels_version("casper-hansen/AutoAWQ_kernels")
+        python_version = "".join(platform.python_version_tuple()[:2])
+        platform_name = platform.system().lower()
+        architecture = platform.machine().lower()
+        latest_rocm_kernels_wheels = get_kernels_whl_url(
+            gpu_system_version,
+            kernels_version,
+            python_version,
+            platform_name,
+            architecture,
+        )
+        requirements.append(f"autoawq-kernels@{latest_rocm_kernels_wheels}")
+    else:
+        raise RuntimeError(
+            "Your system must have either Nvidia or AMD GPU to build this package."
+        )
 
 setup(
     packages=find_packages(),
