79639efe33ee18f6a727e386afdd285b8637884d
VictorSanh <victorsanh@gmail.com>
2024-02-23 23:36:05 +0000

    idefics2

--- awq/modules/fused/model.py
+++ awq/modules/fused/model.py
@@ -129,6 +129,68 @@ class LlamaLikeModel(nn.Module):
         )
 
 
+# class IdeficsLikeModel(nn.Module):
+#     """
+#     LlamaLikeModel is intended to be reused across models that have
+#     an architecture that closely resembles Llama, e.g. Mistral and Aquila.
+#     """
+
+#     def __init__(self, vocab_size, blocks, embedding, norm, vision_model, modality_projection, perceiver_resampler):
+#         super().__init__()
+#         self.vocab_size = vocab_size
+#         self.embedding = embedding
+#         self.blocks: List[LlamaLikeBlock] = nn.ModuleList(blocks)
+#         self.norm = norm
+#         self.last_forward_num_tokens = 0
+#         self.vision_model = vision_model
+#         self.modality_projection = modality_projection
+#         self.perceiver_resampler = perceiver_resampler
+
+#     @torch.inference_mode()
+#     def forward(
+#         self,
+#         input_ids: torch.Tensor,
+#         attn_bias=None,
+#         attention_mask=None,
+#         is_causal=None,
+#         *args,
+#         **kwargs,
+#     ):
+#         input_ids, self.last_forward_num_tokens = fused_utils.prepare_input_ids(
+#             input_ids, self.last_forward_num_tokens
+#         )
+#         _bsz, seqlen = input_ids.shape
+
+#         fused_utils.prepare_cache(self.blocks, seqlen)
+
+#         h = self.embedding(input_ids)
+
+#         mask = fused_utils.prepare_attention_mask(
+#             seqlen=seqlen,
+#             start_pos=self.blocks[0].attn.start_pos,
+#             device=input_ids.device,
+#             type_as=h,
+#         )
+
+#         for layer in self.blocks:
+#             h, mask = fused_utils.prepare_correct_devices(
+#                 layer,
+#                 h,
+#                 mask,
+#             )
+#             h, _, past_key_value = layer(
+#                 h, None, attention_mask=mask, is_causal=is_causal
+#             )
+#         h = self.norm(h)
+
+#         return BaseModelOutputWithPast(
+#             last_hidden_state=h,
+#             past_key_values=past_key_value,
+#             hidden_states=(),
+#             attentions=(),
+#         )
+
+
 class MPTModel(nn.Module):
     def __init__(self, vocab_size, blocks, wte, norm_f):
         super().__init__()
