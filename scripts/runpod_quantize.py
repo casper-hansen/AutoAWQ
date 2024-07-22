9a1cced19fb95a51c7287f737a3ae69e0039fc31
Casper <casperbh.96@gmail.com>
2024-07-22 22:55:54 +0200

    Add CLI

--- scripts/runpod_quantize.py
+++ scripts/runpod_quantize.py
@@ -27,44 +27,39 @@ system_storage_gb = 20 # fp16 model is downloaded here
 volume_storage_gb = 20 # quantized model is saved here
 
 # Quantization Parameters
-hf_model_path = "Qwen/Qwen2-0.5B-Instruct"
 quant_name = "qwen2-0.5b-instruct-awq"
-local_save_path = f"/workspace/{quant_name}"
-hf_upload_path = f"casperhansen/{quant_name}"
+local_save_path = f"/workspace/{quant_name}",
+hf_upload_path = f"casperhansen/{quant_name}",
 
-quant_config = {
-    "zero_point": True,
-    "q_group_size": 128,
-    "w_bit": 4,
-    "version": "GEMM"
-}
-model_config = {
-    "low_cpu_mem_usage": True,
-    "use_cache": False,
-}
+cli_args = dict(
+    hf_model_path = "Qwen/Qwen2-0.5B-Instruct",
+    quant_name = quant_name,
+    local_save_path = local_save_path,
+    hf_upload_path = hf_upload_path,
+    zero_point = True,
+    q_group_size = 128,
+    w_bit = 4,
+    version = "GEMM",
+    low_cpu_mem_usage = True,
+    use_cache = False,
+)
+cli_args = " ".join([f"--{k} {v}" for k,v in cli_args.items()])
 
 docker_command = (
     "bash -c '" +
     "cd /workspace && " +
     "git clone https://github.com/casper-hansen/AutoAWQ.git && " +
+    "git checkout runpod_launch_script && " +
     "cd AutoAWQ && " +
     "pip install -e . && " +
-    "cd .. && " +
     "huggingface-cli login --token $HF_TOKEN && " +
-    "python -c \"" +
-    "import torch; " +
-    "from awq import AutoAWQForCausalLM; " +
-    "from transformers import AutoTokenizer; " +
-    f"model = AutoAWQForCausalLM.from_pretrained('{hf_model_path}', **{model_config}); " +
-    f"tokenizer = AutoTokenizer.from_pretrained('{hf_model_path}', trust_remote_code=True); " +
-    f"model.quantize(tokenizer, quant_config={quant_config}); " +
-    f"model.save_quantized('{local_save_path}'); " +
-    f"tokenizer.save_pretrained('{local_save_path}');" +
-    "\" && " +
+    f"python examples/cli.py {cli_args} && " +
     f"huggingface-cli upload {hf_upload_path} {local_save_path} ./ && " +
     "runpodctl stop pod $RUNPOD_POD_ID" +
     "'"
 )
+
+print(docker_command); exit(0)
 
 template = runpod.create_template(
     name=template_name,
@@ -76,20 +71,18 @@ template = runpod.create_template(
     ports="8888/http,22/tcp",
 )
 
-print(template)
+# pod = runpod.create_pod(
+#     name=template_name,
+#     image_name=docker_image,
+#     template_id=template["id"],
+#     gpu_type_id=gpu_id,
+#     gpu_count=num_gpus,
+#     min_memory_in_gb=system_memory_gb,
+#     volume_in_gb=volume_storage_gb,
+#     container_disk_in_gb=system_storage_gb,
+#     env=env_variables,
+#     volume_mount_path="/workspace",
+#     cloud_type="SECURE",
+# )
 
-pod = runpod.create_pod(
-    name=template_name,
-    image_name=docker_image,
-    template_id=template["id"],
-    gpu_type_id=gpu_id,
-    gpu_count=num_gpus,
-    min_memory_in_gb=system_memory_gb,
-    volume_in_gb=volume_storage_gb,
-    container_disk_in_gb=system_storage_gb,
-    env=env_variables,
-    volume_mount_path="/workspace",
-    cloud_type="SECURE",
-)
-
-print(pod)
+# print(pod)
