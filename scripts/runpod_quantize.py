2efad6aa95624c5d05552f5248146b9f72eb5c78
Casper <casperbh.96@gmail.com>
2024-07-23 16:43:40 +0200

    Better separation of bash commands

--- scripts/runpod_quantize.py
+++ scripts/runpod_quantize.py
@@ -23,14 +23,19 @@ env_variables = {
 gpu_id = gpu_ids["4090"]
 num_gpus = 1
 system_memory_gb = 100
-system_storage_gb = 20 # fp16 model is downloaded here
-volume_storage_gb = 20 # quantized model is saved here
+system_storage_gb = 150 # fp16 model is downloaded here
+volume_storage_gb = 50 # quantized model is saved here
 
 # Quantization Parameters
 hf_model_path = "Qwen/Qwen2-0.5B-Instruct"
 quant_name = "qwen2-0.5b-instruct-awq"
 local_save_path = f"/workspace/{quant_name}"
 hf_upload_path = f"casperhansen/{quant_name}"
+INSTALL_TRANSFORMERS_MAIN = False
+USE_HF_TRANSFER = False
+
+if USE_HF_TRANSFER:
+    env_variables["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 cli_args = dict(
     hf_model_path = hf_model_path,
@@ -45,18 +50,22 @@ cli_args = dict(
 )
 cli_args = " ".join([f"--{k}" if isinstance(v, bool) else f"--{k} {v}" for k,v in cli_args.items()])
 
-docker_command = (
-    "bash -c '" +
-    "cd /workspace && " +
-    "git clone https://github.com/casper-hansen/AutoAWQ.git && " +
-    "cd AutoAWQ && " +
-    "pip install -e . && " +
-    "huggingface-cli login --token $HF_TOKEN && " +
-    f"python examples/cli.py {cli_args} && " +
-    f"huggingface-cli upload {hf_upload_path} {local_save_path} ./ && " +
-    "runpodctl stop pod $RUNPOD_POD_ID" +
-    "'"
-)
+commands = [
+    "cd /workspace",
+    "git clone https://github.com/casper-hansen/AutoAWQ.git",
+    "cd AutoAWQ",
+    "pip install -e .",
+    "pip install -U git+https://github.com/huggingface/transformers.git" if INSTALL_TRANSFORMERS_MAIN else "",
+    "pip install hf-transfer" if USE_HF_TRANSFER else "",
+    "huggingface-cli login --token $HF_TOKEN",
+    f"python examples/cli.py {cli_args}",
+    f"huggingface-cli upload {hf_upload_path} {local_save_path} ./",
+    "runpodctl stop pod $RUNPOD_POD_ID",
+]
+commands = [cmd for cmd in commands if cmd]
+commands = " && ".join(commands)
+
+docker_command = "bash -c '" + commands + "'"
 
 template = runpod.create_template(
     name=template_name,
