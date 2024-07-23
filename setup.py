eb2de41299d483115f3c14c45caed5bed772cacf
Casper <casperbh.96@gmail.com>
2024-07-23 17:53:23 +0200

    Remove python 3.12 classification

--- setup.py
+++ setup.py
@@ -83,7 +83,6 @@ common_setup_kwargs = {
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
         "Programming Language :: C++",
     ],
 }
