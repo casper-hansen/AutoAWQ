b2a5f8d7474d3aeb942164c6a5aa49730846770f
Radi Cho <radicho123@gmail.com>
2024-08-01 02:24:45 +0300

    Add Gemma2 support.

--- awq/models/__init__.py
+++ awq/models/__init__.py
@@ -15,6 +15,7 @@ from .llava import LlavaAWQForCausalLM
 from .mixtral import MixtralAWQForCausalLM
 from .qwen2 import Qwen2AWQForCausalLM
 from .gemma import GemmaAWQForCausalLM
+from .gemma2 import Gemma2AWQForCausalLM
 from .stablelm import StableLmAWQForCausalLM
 from .starcoder2 import Starcoder2AWQForCausalLM
 from .llava_next import LlavaNextAWQForCausalLM
