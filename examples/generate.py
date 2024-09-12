4f3606b500521d864eede8ec252920510da546c5
Casper <casperbh.96@gmail.com>
2024-09-12 12:17:16 +0200

    Update generation example

--- examples/generate.py
+++ examples/generate.py
@@ -1,42 +1,37 @@
+import torch
 from awq import AutoAWQForCausalLM
-from awq.utils.utils import get_best_device
 from transformers import AutoTokenizer, TextStreamer
 
-
-quant_path = "casperhansen/llama-3-8b-instruct-awq"
-
-# Load model
-if get_best_device() == "cpu":
-    model = AutoAWQForCausalLM.from_quantized(quant_path, use_qbits=True, fuse_layers=False)
-else:
-    model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=True)
-tokenizer = AutoTokenizer.from_pretrained(quant_path, trust_remote_code=True)
+model_id = "hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
-prompt = "You're standing on the surface of the Earth. "\
-        "You walk one mile south, one mile west and one mile north. "\
-        "You end up exactly where you started. Where are you?"
-
-chat = [
-    {"role": "system", "content": "You are a concise assistant that helps answer questions."},
-    {"role": "user", "content": prompt},
-]
-
-terminators = [
-    tokenizer.eos_token_id,
-    tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
-
-tokens = tokenizer.apply_chat_template(
-    chat,
-    return_tensors="pt"
+model = AutoAWQForCausalLM.from_quantized(
+  model_id,
+  torch_dtype=torch.float16,
+  low_cpu_mem_usage=True,
+  device_map="auto",
 )
-tokens = tokens.to(get_best_device())
 
-# Generate output
-generation_output = model.generate(
-    tokens,
-    streamer=streamer,
-    max_new_tokens=64,
-    eos_token_id=terminators
+prompt = [
+  {"role": "system", "content": "You are a helpful assistant, that responds as a pirate."},
+  {"role": "user", "content": \
+        "You're standing on the surface of the Earth. "\
+        "You walk one mile south, one mile west and one mile north. "\
+        "You end up exactly where you started. Where are you?"},
+]
+inputs = tokenizer.apply_chat_template(
+  prompt,
+  tokenize=True,
+  add_generation_prompt=True,
+  return_tensors="pt",
+  return_dict=True,
+).to("cuda")
+
+outputs = model.generate(
+    **inputs,
+    do_sample=True,
+    max_new_tokens=256,
+    steamer=streamer,
+    eos_token_id=[tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
 )
