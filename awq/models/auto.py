ce398aba41bb48d5ccdf9efdfb20c97c0722b2f4
Casper <casperbh.96@gmail.com>
2025-01-20 09:21:44 +0100

    add ability to define torch_dtype (#697)

--- awq/models/auto.py
+++ awq/models/auto.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import logging
 from transformers import AutoConfig
 from awq.models import *
@@ -63,6 +64,7 @@ class AutoAWQForCausalLM:
     def from_pretrained(
         self,
         model_path,
+        torch_dtype=torch.float16,
         trust_remote_code=True,
         safetensors=True,
         device_map=None,
@@ -78,6 +80,7 @@ class AutoAWQForCausalLM:
         return AWQ_CAUSAL_LM_MODEL_MAP[model_type].from_pretrained(
             model_path,
             model_type,
+            torch_dtype=torch_dtype,
             trust_remote_code=trust_remote_code,
             safetensors=safetensors,
             device_map=device_map,
