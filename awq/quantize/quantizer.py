8517de84f75d3121eb77a04f6cf0088830899ffd
zju-stu-lizheng <lizheng.cs@zju.edu.cn>
2025-04-25 22:30:54 +0800

    Fixed bugs(attention mask & zero x).

--- awq/quantize/quantizer.py
+++ awq/quantize/quantizer.py
@@ -615,6 +615,8 @@ class AwqQuantizer:
             layer_kwargs["attention_mask"] = layer_kwargs["attention_mask"].to(
                 best_device
             )
+        elif "qwen3" in self.awq_model.model_type:
+            layer_kwargs["attention_mask"] = None
 
         return modules, layer_kwargs, inps
 
